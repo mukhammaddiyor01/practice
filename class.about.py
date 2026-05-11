2afb473b635b90570223a4fe7d76f4450f311968
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-05-11 22:21:09 +0900

    fix: pyhton special/magic methods

--- class.about.py
+++ class.about.py
@@ -51,3 +51,48 @@ print("new_message:", new_message)
 
 # static method|
 Person.explain()
+
+
+print(" ===== (3) special/magic methods =====")
+# Python's most common special methods are below:
+# __init__, __new__, __str__, __call__, __getitem__, __len__, __eq__
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args):    # bu yerda ko'zga ko'rinmasa ham doim __new__ syntax mavjud bo'ladi
+        print("*__new__*")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+
+    def start_engine(self):
+        print(f"the {self.name} started the engine!")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine!")
+
+    def __str__(self):    # bu orqali print(your_car) qilganda o'zimiz xohlagan message chiqarishimiz mumkin ekan
+        return f"the car.name: {self.name} was produced in {self.year} year!"
+
+    def __call__(self):  # __call__ orqali biz methodni functiondek chaqira olamiz ekan
+        print("Object called as function!")
+
+
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+
+print("------")
+your_car = Car("Toyota", 2026)
+print(your_car)
+respnse = your_car()  # look like function
+print("response:", respnse)
