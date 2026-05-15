a9af8f3edff5caed6c54f4112cbb56dbb49dbda8
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-05-15 23:27:18 +0900

    feat: practice lambda function

--- lists.py
+++ lists.py
@@ -115,3 +115,27 @@ numbs = [2, 20, 12, 100]
 new_numbs = sorted(numbs)
 # terminal: the sorted numbs: [2, 20, 12, 100] and new_numbs: [2, 12, 20, 100]
 print(f"the sorted numbs: {numbs} and new_numbs: {new_numbs}")
+
+
+print("====== (3) Lambda function ======")
+# lamba is small anonymous function!
+def calculate(x, y): return x * y
+
+
+result = calculate(3, 5)
+print("result:", result)
+
+
+people = [
+    ("Robert", 20),
+    ("Steve", 19),
+    ("Joseph", 25),
+    ("Michael", 30),
+    ("Ali", 40)
+]
+people.sort()
+print("people(1):", people)
+
+# sort by age via lambda
+people.sort(key=lambda person: person[1])
+print("people(2)", people)
